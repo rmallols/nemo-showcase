1617fb07a25b43e04626f39fc08c1ce46a079e88
rmallols <ricardo.mallols@gmail.com>
2015-08-16 18:28:23 +0100

    Introducing help messages

--- frontend/src/home/HomeCtrl.js
+++ frontend/src/home/HomeCtrl.js
@@ -10,18 +10,7 @@ app
             require: 'nemoFormHandler',
             link: function (scope, element, attrs, formHandlerCtrl) {
 
-                var iconVisibilityStates = {};
-
-                scope.fakeSubmit = function () {
-                    Stats.submitvalidationTracking(formHandlerCtrl.getValidationTracking());
-                    formHandlerCtrl.validateFormAndSetDirtyTouched();
-                    if (scope.isFormValid()) {
-                        formHandlerCtrl.forceInvalid('captcha.invalid');
-                        formHandlerCtrl.giveFirstInvalidFieldFocus();
-                    } else {
-                        formHandlerCtrl.giveFirstInvalidFieldFocus();
-                    }
-                };
+                var iconVisibilityStates = {}, messageTypes = { error: 'error', help: 'help' };
 
                 scope.isFormValid = formHandlerCtrl.isFormValid;
 
@@ -32,26 +21,33 @@ app
                     };
                 };
 
-                scope.onErrorIconHover = function (fieldName) {
+                scope.getMessageType = function (fieldName) {
+                    var iconType,
+                        isInvalid = !formHandlerCtrl.isFieldValid(fieldName),
+                        isTouched = formHandlerCtrl.isFieldTouched(fieldName),
+                        hasHelp = formHandlerCtrl.hasHelp(fieldName);
+                    if(isInvalid && isTouched) {
+                        iconType = messageTypes.error;
+                    } else if(hasHelp) {
+                        iconType = messageTypes.help;
+                    }
+                    return iconType;
+                };
+
+                scope.onIconHover = function (fieldName) {
                     iconVisibilityStates[fieldName] = true;
                 };
 
-                scope.onErrorIconBlur = function (fieldName) {
+                scope.onIconBlur = function (fieldName) {
                     iconVisibilityStates[fieldName] = false;
                 };
 
-                scope.isErrorIconVisible = function (fieldName) {
-                    var isInvalid = !formHandlerCtrl.isFieldValid(fieldName),
-                        isTouched = formHandlerCtrl.isFieldTouched(fieldName);
-                    return isInvalid && isTouched;
+                scope.isErrorMessageVisible = function (fieldName) {
+                    return isMessageVisible(fieldName, messageTypes.error);
                 };
 
-                scope.isErrorMessageVisible = function (fieldName) {
-                    var isInvalid = !formHandlerCtrl.isFieldValid(fieldName),
-                        isActive = formHandlerCtrl.isFieldActive(fieldName),
-                        isTouched = formHandlerCtrl.isFieldTouched(fieldName),
-                        isErrorIconHovered = iconVisibilityStates[fieldName];
-                    return isInvalid && (isErrorIconHovered || (isActive && isTouched));
+                scope.isHelpMessageVisible = function (fieldName) {
+                    return isMessageVisible(fieldName, messageTypes.help);
                 };
 
                 scope.isHoveredAndNotActive = function (fieldName) {
@@ -59,6 +55,63 @@ app
                 };
 
                 scope.getFieldNgModelCtrl = formHandlerCtrl.getFieldNgModelCtrl;
+
+                function isMessageVisible(fieldName, messageType) {
+                    var currentMessageType = scope.getMessageType(fieldName),
+                        isActive = formHandlerCtrl.isFieldActive(fieldName),
+                        isIconHovered = iconVisibilityStates[fieldName];
+                    return currentMessageType === messageType && (isActive || isIconHovered);
+                }
+
+                //var iconVisibilityStates = {};
+                //
+                //scope.fakeSubmit = function () {
+                //    Stats.submitvalidationTracking(formHandlerCtrl.getValidationTracking());
+                //    formHandlerCtrl.validateFormAndSetDirtyTouched();
+                //    if (scope.isFormValid()) {
+                //        formHandlerCtrl.forceInvalid('captcha.invalid');
+                //        formHandlerCtrl.giveFirstInvalidFieldFocus();
+                //    } else {
+                //        formHandlerCtrl.giveFirstInvalidFieldFocus();
+                //    }
+                //};
+                //
+                //scope.isFormValid = formHandlerCtrl.isFormValid;
+                //
+                //scope.getFieldStyleClasses = function (fieldName) {
+                //    return {
+                //        'ng-touched': formHandlerCtrl.isFieldTouched(fieldName),
+                //        'ng-invalid': !formHandlerCtrl.isFieldValid(fieldName)
+                //    };
+                //};
+                //
+                //scope.onErrorIconHover = function (fieldName) {
+                //    iconVisibilityStates[fieldName] = true;
+                //};
+                //
+                //scope.onErrorIconBlur = function (fieldName) {
+                //    iconVisibilityStates[fieldName] = false;
+                //};
+                //
+                //scope.isErrorIconVisible = function (fieldName) {
+                //    var isInvalid = !formHandlerCtrl.isFieldValid(fieldName),
+                //        isTouched = formHandlerCtrl.isFieldTouched(fieldName);
+                //    return isInvalid && isTouched;
+                //};
+                //
+                //scope.isErrorMessageVisible = function (fieldName) {
+                //    var isInvalid = !formHandlerCtrl.isFieldValid(fieldName),
+                //        isActive = formHandlerCtrl.isFieldActive(fieldName),
+                //        isTouched = formHandlerCtrl.isFieldTouched(fieldName),
+                //        isErrorIconHovered = iconVisibilityStates[fieldName];
+                //    return isInvalid && (isErrorIconHovered || (isActive && isTouched));
+                //};
+                //
+                //scope.isHoveredAndNotActive = function (fieldName) {
+                //    return iconVisibilityStates[fieldName] && !formHandlerCtrl.isFieldActive(fieldName);
+                //};
+                //
+                //scope.getFieldNgModelCtrl = formHandlerCtrl.getFieldNgModelCtrl;
             }
         }
     }]);
